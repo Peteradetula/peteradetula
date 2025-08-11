42f18149ac05d1ed89ae7668a21ce604154ed50d
Sunday Oruwhone <sundayoruwhone@gmail.com>
2025-08-11 20:08:15 +0100

    Fixed Contacts page layout content.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
 import ContactForm from '@/components/contactpage/ContactForm'
+import LetsChat from '@/components/LetsChat/LetsChat'
 import CTA from '@/components/shared/CTA'
 import CtaImageSlider from '@/components/shared/CtaImageSlider'
 import LayoutOne from '@/components/shared/LayoutOne'
@@ -14,12 +15,12 @@ const ContactPage = () => {
       <PageHero
         title="Let's Work Together"
         badgeTitle="Contact"
-        description="Discover our innovative, cutting-edge no-code websites, crafted to effortlessly captivate and engage your visitors."
+        description="Discover innovative, cutting-edge solutions crafted to effortlessly captivate and engage your users."
         scale
         spacing="pt-[130px] md:pt-[180px] pb-20 sm:pb-32 md:pb-36 lg:pb-36 xl:pb-[100px] relative overflow-hidden"
       />
       <ContactForm />
-      <CTA>
+      {/* <CTA>
         Let's chat!
         <CtaImageSlider
           slides={[
@@ -30,7 +31,8 @@ const ContactPage = () => {
         />
         with us.
         <i className="block font-instrument italic max-md:inline-block max-sm:pl-2 sm:mt-10">A virtual coffee?</i>
-      </CTA>
+      </CTA> */}
+      <LetsChat />
     </LayoutOne>
   )
 }
